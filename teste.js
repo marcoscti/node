529afa7a22d6c752b1c32f37a804ebd5edc6c5d1
Marcos Cordeiro <marcosc974@gmail.com>
2021-12-29 22:17:30 -0300

    Aprendendo a manipular dados com nodeJS e Sequelize

--- teste.js
+++ teste.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize('test','root','',{
     host: 'localhost',
     dialect: 'mysql'
 });
-
+/*
 sequelize.authenticate().then(
     ()=>{
     console.log("Conectado com sucesso")
@@ -14,3 +14,40 @@ sequelize.authenticate().then(
     (erro)=>{
     console.log('Falha ao se conectar: '+erro)
 })
+*/
+const Postagem = sequelize.define('postagens',{
+    titulo:{
+        type: Sequelize.STRING,
+    },
+    conteudo:{
+        type:Sequelize.TEXT
+    }
+})
+const Usuario = sequelize.define('usuarios',{
+    nome:{
+        type:Sequelize.STRING
+    },
+    sobrenome:{
+        type:Sequelize.STRING
+    },
+    idade:{
+        type:Sequelize.INTEGER
+    },
+    email:{
+        type:Sequelize.STRING
+    }
+})
+
+/*Postagem.create({
+    titulo:"Teste de criação",
+    conteudo: "Craindo conteudos para o nmysql pelo node js"
+})
+*/
+Usuario.create({
+    nome: "Marcos",
+    sobrenome: "Cordeiro",
+    idade:28,
+    email:"marcosc974@gmail.com"
+})
+//Postagem.sync({force:true})
+//Usuario.sync({force:true})
